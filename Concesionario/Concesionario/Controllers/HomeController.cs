08f9d15daa395158743f3ee9df64a6b7465de478
QuimeyEspinosa <quimey.espinosa@hotmail.com>
2021-01-27 10:27:23 -0300

    Controladores y formulario

--- Concesionario/Concesionario/Controllers/HomeController.cs
+++ Concesionario/Concesionario/Controllers/HomeController.cs
@@ -17,5 +17,10 @@ namespace Concesionario.Controllers
         {
             return View();
         }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
     }
 }
