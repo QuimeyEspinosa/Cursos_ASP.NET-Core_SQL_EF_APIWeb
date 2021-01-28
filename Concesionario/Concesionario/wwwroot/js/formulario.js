6b168fad825b6082affc8721c49a2ce359e1c95a
QuimeyEspinosa <quimey.espinosa@hotmail.com>
2021-01-27 23:31:16 -0300

    Form, Js

---
+++ Concesionario/Concesionario/wwwroot/js/formulario.js
@@ -0,0 +1,65 @@
+﻿const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+}
+
+const campos = {
+    name: false,
+    surname: false,
+    email: false
+}
+
+const validarForm = (e) => {
+
+    switch (e.target.name) {
+
+        case "name":
+            validarCampo(expresiones.nombre, e.target, 'name');
+            break;
+
+        case "surname":
+            validarCampo(expresiones.nombre, e.target, 'surname');
+            break;
+
+        case "email":
+            validarCampo(expresiones.correo, e.target, 'email');
+            break;
+    }
+}
+
+const validarCampo = (expresion, input, campo) => {
+
+    if (expresion.test(input.value)) {
+        document.getElementById(campo).classList.remove("input-incorrect");
+        document.getElementById(campo).classList.add("input-correct");
+        campos[campo] = true;
+    }
+    else {
+        document.getElementById(campo).classList.add("input-incorrect");
+        campos[campo] = false;
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', validarForm); //key up ejecuta la función de validar cuando se suelta una tecla
+    input.addEventListener('blur', validarForm); //blur ejecuta la función de validar cuando se clickee fuera del input
+});
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault(); //previene enviar los datos
+
+    if (campos.name && campos.surname && campos.email) {
+        formulario.reset();
+        document.getElementById("form-message").classList.remove('form-message-active');
+        document.getElementById("form-message").classList.add('form-message');
+    }
+    else {
+        document.getElementById("form-message").classList.add('form-message-active');
+    }
+});
---
+++ Concesionario/Concesionario/wwwroot/js/slider.js
@@ -0,0 +1,11 @@
+﻿window.addEventListener('load', function () {
+    new Glider(document.querySelector('.slider-list'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: '.slider-indicators',
+        arrows: {
+            prev: '.slider-prev',
+            next: '.slider-next'
+        }
+    });
+})
